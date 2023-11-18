9bc334a60661155e993571d65c0776bff15eb724
Prerana <43247643+rprerana@users.noreply.github.com>
2023-11-18 17:17:49 -0500

    Update AppWeek13.py

--- AppWeek13.py
+++ AppWeek13.py
@@ -7,7 +7,7 @@ import numpy as np
 
 df_IMDB_1=pd.read_csv('IMDB_Dataset_50KPolarity_pt1.csv')
 df_IMDB_2=pd.read_csv('IMDB_Dataset_50KPolarity_pt2.csv')
-df_IMDB_new = pd.concat([df_IMDB_1,df_IMDB_2])
+df_IMDB_new = pd.concat([df_IMDB_1,df_IMDB_2], axis = 0)
 
 fig1 = px.histogram(df_IMDB_new, x='subjectivity', color= "sentiment", title="Subjectivity of positive and negative reviews")
 fig1.update_layout(template='plotly_white')
