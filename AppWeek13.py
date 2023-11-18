58a312d825067dcbab4a684a7264cbefa93e90b8
Prerana <43247643+rprerana@users.noreply.github.com>
2023-11-18 17:13:00 -0500

    Update AppWeek13.py

--- AppWeek13.py
+++ AppWeek13.py
@@ -9,16 +9,17 @@ df_IMDB_1=pd.read_csv('IMDB_Dataset_50KPolarity_pt1.csv')
 df_IMDB_2=pd.read_csv('IMDB_Dataset_50KPolarity_pt2.csv')
 df_IMDB_new = pd.concat([df_IMDB_1,df_IMDB_2],ignore_index = True)
 
-df_IMDB_positive=df_IMDB_new[df_IMDB_new['sentiment']==1]
-df_IMDB_negative=df_IMDB_new[df_IMDB_new['sentiment']==0]
-
 fig1 = px.histogram(df_IMDB_new, x='subjectivity', color= "sentiment", title="Subjectivity of positive and negative reviews")
 fig1.update_layout(template='plotly_white')
 fig2 = px.histogram(df_IMDB_new, x='polarity', color= "sentiment", title="Polarity of positive and negative reviews")
 fig2.update_layout(template='plotly_white')
 
-st.title('Quantitative Question')
-st.subheader(':red[This is a string that explains something above.]')
-
-st.plotly_chart(fig1)
+st.title(':blue[Quantitative Question]')
+st.subheader(':red[How does the distribution of sentiment polarity scores vary between positive and negative movie reviews?]')
 st.plotly_chart(fig2)
+
+st.subheader(':red[How does the distribution of subjectivity differ between positive and negative movie reviews?]')
+st.plotly_chart(fig1)
+
+st.title(':blue[Data]')
+st.dataframe(df_IMDB_new)  
